21fe8e7a1b9d7fdbac5c1a2a8edd43bf69758e37
Rodrigo Riveros <riverosdev@gmail.com>
2023-11-24 11:19:41 -0300

    subo proyecto completo

---
+++ src/routes/api.v1/movies.routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { list, detail, create, update, destroy } = require('../../controllers/moviesController')
+const router = express.Router()
+
+// /api/v1/movies
+
+router
+    .get('/', list)
+    .get('/:id', detail)
+    .post('/', create)
+    .put('/:id', update)
+    .delete('/:id', destroy)
+
+module.exports = router
